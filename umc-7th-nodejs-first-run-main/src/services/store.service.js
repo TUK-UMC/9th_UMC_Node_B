e1ef33accea4ffacec471d63e72b75985a99da99
이예빈 <toyou@tukorea.ac.kr>
2025-11-10 00:53:12 +0900

    Delete umc-7th-nodejs-first-run-main directory

--- umc-7th-nodejs-first-run-main/src/services/store.service.js
+++
@@ -1,8 +0,0 @@
-import { addStoreToDB } from "../repositories/store.repository.js";
-import { responseFromStore } from "../dtos/store.dto.js";
-
-export const addStore = async (storeDTO) => {
-  const createdStore = await addStoreToDB(storeDTO);
-
-  return responseFromStore(createdStore);
-};
